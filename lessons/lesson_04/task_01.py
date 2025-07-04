649d35ce52838c8cc9ca616c78d600424adaa9c6
orekhov-vitaly <orekhov.vitaly@gmail.com>
2025-07-04 22:56:02 +0200

    Lesson 4

---
+++ lessons/lesson_04/task_01.py
@@ -0,0 +1,14 @@
+greeting  = "Привет "
+number = 10
+result = greeting + str(number)
+
+print(result)
+
+print("======\n")
+
+s = "I know:"
+lang1 = "Python"
+lang2 = "Java"
+lang3 = "JavaScript"
+
+print(s, lang1, lang2, lang3)
---
+++ lessons/lesson_04/task_02.py
@@ -0,0 +1,3 @@
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+print("Hello,", user_name, "Вам", user_age , "л.", "Через год Вам будет", int(user_age) + 1)
