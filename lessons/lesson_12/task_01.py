8d4742d567bd1aa40b473b04068c7533e5a732d3
orekhov-vitaly <orekhov.vitaly@gmail.com>
2025-07-17 20:58:42 +0200

    Lesson 12

---
+++ lessons/lesson_12/task_01.py
@@ -0,0 +1,10 @@
+print("===== while =====")
+i = 0
+
+while i < 5:
+    print("Hello!", i)
+    i += 1
+
+print("\n===== for =====")
+for i in range(5):
+    print("Hello!", i)
---
+++ lessons/lesson_12/task_02.py
@@ -0,0 +1,12 @@
+i = 0
+
+while i < 10:
+    print(i)
+    i += 1
+
+print("========")
+
+for j in range(10):
+    print(j)
+
+print(j)
