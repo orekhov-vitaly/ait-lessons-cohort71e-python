6225bb4b121dca052377f3e9fe3e22b25f5bba32
orekhov-vitaly <orekhov.vitaly@gmail.com>
2025-07-17 20:59:19 +0200

    Lesson 13

---
+++ lessons/lesson_13/task_01.py
@@ -0,0 +1,22 @@
+for number in range(5):
+    print(number)
+
+print()
+
+for number in range(3, 8):
+    print(number)
+
+print()
+
+for number in range(5, 23, 3):
+    print(number)
+
+print()
+
+for number in range(7, 2, -1):
+    print(number)
+
+print()
+
+for number in range(100, 75, -5):
+    print(number)
